6190ab1a04b3d93ac9b139b96ca04e2191646ece
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-05-23 17:56:51 -0400

    Minor fix to label, added add address and phone number buttons, removed span for phone number and replaced with ul in index.html. Added phoneaddressfields variable to limit add address and phone number to one additional field, added addaddress addphonenumber functions, removed neighborphonenumber.html and changed to setphonenumbers function in contactlinks function, added additional fields to createnewcontact function, added setphonenumbers function, added additional fields to clearfields, added additional fields to contact object, added getphonenumber2 getstreet2 getcity2 and getstate2 functions, added additional fields to addcontact function, added fields to findcontact function.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 $('document').ready(function() {
   var contacts = new MapOfContacts();
+  //keep track of how many phone number and address fields there are
+  var phoneAddressFields = [1, 1];
   /*
   * Function to create new map of contact with first contact entered in. Also
   * sets links in contacts section
@@ -10,15 +12,35 @@ $('document').ready(function() {
     setContactLinks();
     e.preventDefault();
   });
+  $('#addAddress').click(function(e) {
+    //limit address fields to 2
+    if (phoneAddressFields[0] === 1) {
+      $('#addAddress').prev('div').append('<div class="form-group" id="address2"><label ' +
+      'for="inputStreet2">Street</label><input type="street" class="form-control"' +
+      ' id="inputStreet2"></div><div class="form-group"><label for="inputCity2">City' +
+      '</label><input type="city" class="form-control" id="inputCity2"></div><div' +
+      ' class="form-group"><label for="inputState2">State</label><input type="lastState"' +
+      ' class="form-control" id="inputState2"></div>');
+      phoneAddressFields[0]++;
+    }
+  });
+  $('#addPhoneNumber').click(function(e) {
+    //limit phone number fields to 2
+    if (phoneAddressFields[1] === 1) {
+      $('#addPhoneNumber').parent().parent().append('<div class="form-group" id="phone2">' +
+      '<label for="inputPhoneNumber2">Phone Number</label><input type="phoneNumber"' +
+      ' class="form-control" id="inputPhoneNumber2"></div>');
+      phoneAddressFields[1]++;
+    }
+  });
   /*
   * Function to add nextdoor neighbor information along with addresses
   */
   $('#contactsLinks').on('click', 'li a', function(e) {
     var fullContact = contacts.findContact($(e.target).text());
-    var blah = fullContact[0];
     $('#neighborFirstName').html(fullContact[0]);
     $('#neighborLastName').html(fullContact[1]);
-    $('#neighborPhoneNumber').html(fullContact[2]);
+    setPhoneNumbers(fullContact);
     setAddresses(fullContact);
     e.preventDefault();
   });
@@ -26,8 +48,10 @@ $('document').ready(function() {
   * Function to create a new contact
   */
   function createNewContact() {
-    contacts.addContact($('#inputFirstName').val(), $('#inputLastName').val(), $('#inputPhoneNumber').val(),
-    $('#inputStreet').val(), $('#inputCity').val(), $('#inputState').val());
+    contacts.addContact($('#inputFirstName').val(), $('#inputLastName').val(),
+    $('#inputPhoneNumber').val(), $('#inputStreet').val(), $('#inputCity').val(),
+    $('#inputState').val(), $('#inputPhoneNumber2').val(), $('#inputStreet2').val(),
+    $('#inputCity2').val(), $('#inputState2').val());
   }
   /*
   * function to iterate and set the contact links
@@ -40,36 +64,62 @@ $('document').ready(function() {
     }
   }
   /*
+  * Function to iterate and set phone numbers
+  */
+  function setPhoneNumbers(list) {
+    $('#neighborPhoneNumber').html('');
+    for (i = 2, j = 0; j < 2; i++, j++) {
+      if (list[i] !== null && list[i] !== undefined){
+        $('#neighborPhoneNumber').append('<li>' + list[i] + '</li>');
+      }
+    }
+  }
+  /*
   * Function to iterate and set the addresses
   */
   function setAddresses(list) {
     $('#neighborAddressList').html('');
-    for (i = 3; i < list.length - 3 / 3; i += 3){
-      $('#neighborAddressList').append('<li>' + list[i] + ', ' + list[i + 1] + ', ' + list[i + 2] +'</li>');
+    for (i = 4, j = 0; j < 2; i += 3, j++){
+      if (list[i] !== null && list[i] !== undefined){
+        $('#neighborAddressList').append('<li>' + list[i] + ', ' + list[i + 1] + ', ' + list[i + 2] + '</li>');
+      }
     }
+  }
+  /*
+  * Function to clear all of the fields
+  */
+  function clearFields() {
+    $('#inputFirstName').val('');
+    $('#inputLastName').val('');
+    $('#inputPhoneNumber').val('');
+    $('#inputStreet').val('');
+    $('#inputCity').val('');
+    $('#inputState').val('');
+    $('#inputPhoneNumber2').val('');
+    $('#inputStreet2').val('');
+    $('#inputCity2').val('');
+    $('#inputState2').val('');
+    $('#phone2').remove();
+    $('#address2').remove();
+    phoneAddressFields[0] = 1;
+    phoneAddressFields[1] = 1;
   }
 });
 /*
-* Function to clear all of the fields
-*/
-function clearFields() {
-  $('#inputFirstName').val('');
-  $('#inputLastName').val('');
-  $('#inputPhoneNumber').val('');
-  $('#inputStreet').val('');
-  $('#inputCity').val('');
-  $('#inputState').val('');
-}
-/*
 * Contact object for map
 */
-function Contact(firstName, lastName, phoneNumber, street, city, state) {
+function Contact(firstName, lastName, phoneNumber, street, city, state, phoneNumber2,
+  street2, city2, state2) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
   this.street = street;
   this.city = city;
   this.state = state;
+  this.phoneNumber2 = phoneNumber2;
+  this.street2 = street2;
+  this.city2 = city2;
+  this.state2 = state2;
 }
 /*
 * Function to get first name
@@ -108,6 +158,30 @@ Contact.prototype.getState = function() {
   return this.state;
 };
 /*
+* Function to get phone number 2
+*/
+Contact.prototype.getPhoneNumber2 = function() {
+  return this.phoneNumber2;
+};
+/*
+* Function to get street address 2
+*/
+Contact.prototype.getStreet2 = function() {
+  return this.street2;
+};
+/*
+* Function to get city 2
+*/
+Contact.prototype.getCity2 = function() {
+  return this.city2;
+};
+/*
+* Function to get state 2
+*/
+Contact.prototype.getState2 = function() {
+  return this.state2;
+};
+/*
 * Map object
 */
 function MapOfContacts() {
@@ -116,8 +190,10 @@ function MapOfContacts() {
 /*
 * Function to addContact
 */
-MapOfContacts.prototype.addContact = function(firstName, lastName, phoneNumber, street, city, state) {
-  this.contacts.set((firstName.concat(' ' + lastName)), new Contact(firstName, lastName, phoneNumber, street, city, state));
+MapOfContacts.prototype.addContact = function(firstName, lastName, phoneNumber,
+  street, city, state, phoneNumber2, street2, city2, state2) {
+  this.contacts.set((firstName.concat(' ' + lastName)), new Contact(firstName,
+    lastName, phoneNumber, street, city, state, phoneNumber2, street2, city2, state2));
 };
 /*
 * Function to get the first name and last name for the links
@@ -138,8 +214,12 @@ MapOfContacts.prototype.findContact = function(name) {
   fullContact.push(temp.getFirst());
   fullContact.push(temp.getLastName());
   fullContact.push(temp.getPhoneNumber());
+  fullContact.push(temp.getPhoneNumber2());
   fullContact.push(temp.getStreet());
   fullContact.push(temp.getCity());
   fullContact.push(temp.getState());
+  fullContact.push(temp.getStreet2());
+  fullContact.push(temp.getCity2());
+  fullContact.push(temp.getState2());
   return fullContact;
 };
